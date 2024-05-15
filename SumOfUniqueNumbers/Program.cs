77b38dff12fc034e2a31f0d625582c1c4159e1ed
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2024-05-15 21:03:58 +0500

    Добавьте файлы проекта.

---
+++ SumOfUniqueNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfUniqueNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//принимаем массив чисел с заданой нужной суммой
+			int[] nums = { 1, 2, 3, 4, 5, 6,7,8,9,10};
+			int targetSum = 8;
+			
+			FindUniqueSum(nums, targetSum);
+
+			
+			Console.ReadKey();
+		}
+		static void FindUniqueSum(int[] nums,int targetSum)
+		{
+			//сумируем каждое число с каждым по принципу текущее с остальными следующими
+			int[] proccessedNums = new int[nums.Length];
+			for (int j = 0; j < nums.Length - 1; j++)
+			{
+				for (int i = j; i < nums.Length - 1; i++)
+				{
+					proccessedNums[i] = nums[j] + nums[i + 1];
+					Console.WriteLine(nums[j] + "+" + nums[i + 1] + "=" + proccessedNums[i]);
+					//находим число равное нужной сумме
+					if (proccessedNums[i] == targetSum)
+					{
+						Console.WriteLine("True");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
